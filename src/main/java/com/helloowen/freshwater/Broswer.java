228450b7b6ee2e4492ab09d67f8ae063b8a1f1ce
Owen Tsao <richard.tsao@live.cn>
2016-05-15 15:53:28 +1200

    Added Browser in the framework.

--- src/main/java/com/helloowen/freshwater/Broswer.java
+++ src/main/java/com/helloowen/freshwater/Broswer.java
@@ -1,7 +1,23 @@
 package com.helloowen.freshwater;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 /**
  * Created by caoyunxiang on 2/20/16.
  */
-public class Broswer {
+public abstract class Broswer {
+    static {
+        System.setProperty("webdriver.chrome.driver", "/Users/caoyunxiang/Dev/AngularMaterialTest/src/chromedriver");
+    }
+
+    static WebDriver webDriver = new ChromeDriver();
+
+    public static WebDriver getBrower() {
+        return webDriver;
+    }
+
+    public static void main(String[] args) {
+        webDriver.get("https://material.angularjs.org/latest/demo/autocomplete");
+    }
 }
